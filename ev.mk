fa1fe1be479f327b3b6c9a1bf0ba25e2c7d64d29
Jmz <jmztaylor@gmail.com>
2012-09-22 06:39:04 -0700

    Change codename
    Change-Id: Idb11b9074a9989384040f8a0a3100ae04ab4810e

--- ev.mk
+++ ev.mk
@@ -18,7 +18,7 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="virgin_mobile/htc_shooter/shooter:4.0.3/IML74K/372137.2:user/release-keys" PRIVATE_BUILD_DESC="1.13.652.2_R2 CL372137 release-keys"
 
 # Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := Neco
+PRODUCT_CODENAME := contendo
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Evo V 4g\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @jmz_software for the latest Evervolv updates\n------------------------------------------------\n"
