403ae057cc11c654f606e159e4c2f88adc5da0de
andrew.boren <andrew.boren@gmail.com>
2012-09-26 20:31:10 -0700

    Revert "Build kernel inline"
    This reverts commit 17321914b31881350a6a9915dbd2eaed2343af55.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,3 @@
 # All we want to do here is inherit from shooter common.
 -include device/htc/shooter/BoardConfig.mk
 
-TARGET_PREBUILT_KERNEL := device/htc/shooter/prebuilt/root/kernel
-TARGET_KERNEL_CONFIG   := shooter_defconfig
-BUILD_KERNEL           := true
